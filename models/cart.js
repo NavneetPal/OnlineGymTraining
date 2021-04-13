784e6682852ab89a545e516dea3d474bb0f8969b
Navneet <navneetpal8106@gmail.com>
2021-04-14 00:48:52 +0530

    add plus to cart added and navabar anchor tag changed

--- models/cart.js
+++ models/cart.js
@@ -1,4 +1,5 @@
 module.exports=function Cart(oldCart){
+    //items contains the object of items with key as their productId and value is and object that contains item price qty
     this.items=oldCart.items||{};
     this.totalQty=oldCart.totalQty|| 0;
     this.totalPrice=oldCart.totalPrice||0;
@@ -20,5 +21,24 @@ module.exports=function Cart(oldCart){
             arr.push(this.items[id]);
         }
         return arr;
+    }
+    //qty price item
+
+    this.reduceByOne=function(id){
+        this.items[id].qty--;
+        this.items[id].price-=this.items[id].item.price;
+        this.totalQty--;
+        this.totalPrice-=this.items[id].item.price;
+
+        if(this.items[id].qty<=0){
+            delete this.items[id];
+        }
+    }
+
+    this.increaseByOne=function(id){
+        this.items[id].qty++;
+        this.items[id].price+=this.items[id].item.price;
+        this.totalQty++;
+        this.totalPrice+=this.items[id].item.price;
     }
 }
--- routes/index.js
+++ routes/index.js
@@ -33,4 +33,24 @@ router.get('/cart',(req,res)=>{
     res.render('checkout',{products:cart.generateArray(),totalPrice:cart.totalPrice});
 })
 
+router.get('/reduce/:id',(req,res)=>{
+    const productId=req.params.id;
+    const cart=new Cart(req.session.cart? req.session.cart:{});
+
+    cart.reduceByOne(productId);
+    req.session.cart=cart;
+    res.redirect('/cart');
+})
+
+router.get('/increase/:id',(req,res)=>{
+    const productId=req.params.id;
+    const cart=new Cart(req.session.cart? req.session.cart:{});
+
+    cart.increaseByOne(productId);
+    req.session.cart=cart;
+    res.redirect('/cart');
+})
+
+
+
 module.exports=router;
